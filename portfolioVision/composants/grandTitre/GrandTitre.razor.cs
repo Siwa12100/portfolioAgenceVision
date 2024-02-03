1d1bbd7dd12ca742a069dca7ba564a88e69857c7
Siwa12100 <jean.marcillac12@gmail.com>
2024-02-04 00:08:48 +0100

    ajout du composant pour les grands titres

---
+++ portfolioVision/composants/grandTitre/GrandTitre.razor.cs
@@ -0,0 +1,19 @@
+﻿
+namespace portfolioVision.composants.grandTitre
+{
+    public partial class GrandTitre
+    {
+        [Microsoft.AspNetCore.Components.Parameter]
+        public String Texte {  get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            if (Texte == null)
+            {
+                Texte = "Pas de titre spécifié...";
+            }
+
+            base.OnInitializedAsync();
+        }
+    }
+}
