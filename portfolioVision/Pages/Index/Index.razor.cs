ee0eb37ad6b9cc32f48279971aa8f5b9212e65a0
Siwa12100 <jean.marcillac12@gmail.com>
2024-02-04 02:35:49 +0100

    creation et design des cartes

---
+++ portfolioVision/Pages/Index/Index.razor.cs
@@ -0,0 +1,59 @@
+﻿namespace portfolioVision.Pages.Index
+{
+    public partial class Index
+    {
+
+
+        private String TitreCarte1;
+        private String TitreCarte2;
+
+        private String PrixCarte1;
+        private String PrixCarte2;
+
+        private List<String> CaracteristiquesCarte1;
+        private List<String> CaracteristiquesCarte2;
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            this.TitreCarte1 = "Format court";
+            this.TitreCarte2 = "Format long";
+
+            this.PrixCarte1 = "19,99€";
+            this.PrixCarte2 = "24,99€";
+
+            this.CaracteristiquesCarte1 = new List<String>();
+            this.CaracteristiquesCarte2 = new List<String>();
+
+            this.CaracteristiquesCarte1.Add("Montage de base (coupe et fusion des rushs)");
+            this.CaracteristiquesCarte1.Add("Etalonnage des couleurs");
+            this.CaracteristiquesCarte1.Add("Conception sonore et mixage");
+            this.CaracteristiquesCarte1.Add("Animations Graphiques");
+            this.CaracteristiquesCarte1.Add("Sous-titrage");
+            this.CaracteristiquesCarte1.Add("Adaptation à la charte graphique");
+            this.CaracteristiquesCarte1.Add("Jusqu'à 15 minutes de rushs fournis");
+            this.CaracteristiquesCarte1.Add("Jusqu'à 2 minutes de durée finale");
+            this.CaracteristiquesCarte1.Add("Livraison sous 48 heures");
+
+            this.CaracteristiquesCarte2.Add("Montage de base (coupe et fusion des rushs)");
+            this.CaracteristiquesCarte2.Add("Etalonnage des couleurs");
+            this.CaracteristiquesCarte2.Add("Conception sonore et mixage");
+            this.CaracteristiquesCarte2.Add("Animations Graphiques");
+            this.CaracteristiquesCarte2.Add("Sous-titrage");
+            this.CaracteristiquesCarte2.Add("Adaptation à la charte graphique");
+
+
+
+
+
+
+
+            base.OnInitializedAsync();
+        }
+
+        private async Task InitialisationCartes()
+        {
+
+        }
+    }
+}
--- portfolioVision/composants/carteFortaits/CarteForfaits.razor.cs
+++ portfolioVision/composants/carteFortaits/CarteForfaits.razor.cs
@@ -1,6 +1,45 @@
-﻿namespace portfolioVision.composants.carteFortaits
+﻿using Microsoft.AspNetCore.Components;
+
+namespace portfolioVision.composants.carteFortaits
 {
     public partial class CarteForfaits
     {
+        [Parameter]
+        public String Titre { get; set; }
+
+        [Parameter]
+        public String Prix { get; set; }
+
+        [Parameter]
+        public List<String> Caracteristiques { get; set; }
+
+        //public CarteForfaits(String Titre, String Prix, List<String> Caracteristiques) {
+        
+        //    this.Titre = Titre;
+        //    this.Prix = Prix;
+        //    this.Caracteristiques = Caracteristiques;
+        
+        //}
+
+        protected async override Task OnInitializedAsync()
+        {
+            if (Caracteristiques == null)
+            {
+                Caracteristiques = new List<String>();
+                Caracteristiques.Add("Liste de caractéristiques vide...");
+            }
+
+            if (Titre == null)
+            {
+                Titre = "Pas de titre renseigné...";
+            }
+
+            if (Prix == null)
+            {
+                Prix = "Pas de prix renseigné...";
+            }
+
+            base.OnInitializedAsync();
+        }
     }
 }
